69d73fb4743a12c7ee17fd1efa8c9ec1847bdc94
linxinfa <37791251+linxinfa@users.noreply.github.com>
2020-11-18 08:35:11 +0800

    Unity UIPolygon Demo

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+
+    public Button btn;
+
+    void Start()
+    {
+        btn.onClick.AddListener(() => 
+        {
+            Debug.Log("on btn clicked");
+        });
+    }
+}
---
+++ Assets/Scripts/UIPolygon.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class UIPolygon : Image
+{
+    private PolygonCollider2D _polygon = null;
+    private PolygonCollider2D polygon
+    {
+        get
+        {
+            if (_polygon == null)
+                _polygon = GetComponent<PolygonCollider2D>();
+            return _polygon;
+        }
+    }
+
+    //设置只响应点击，不进行渲染
+    protected UIPolygon()
+    {
+        useLegacyMeshGeneration = true;
+    }
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+    }
+
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        return polygon.OverlapPoint(eventCamera.ScreenToWorldPoint(screenPoint));
+    }
+
+#if UNITY_EDITOR
+    protected override void Reset()
+    {
+        //重置不规则区域
+        base.Reset();
+        transform.position = Vector3.zero;
+        float w = (rectTransform.sizeDelta.x * 0.5f) + 0.1f;
+        float h = (rectTransform.sizeDelta.y * 0.5f) + 0.1f;
+        polygon.points = new Vector2[]
+        {
+            new Vector2(-w,-h),
+            new Vector2(w,-h),
+            new Vector2(w,h),
+            new Vector2(-w,h)
+          };
+    }
+#endif
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(UIPolygon), true)]
+public class UIPolygonInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //什么都不写用于隐藏面板的显示
+    }
+}
+#endif
